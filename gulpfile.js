fd2fb6e6b68cb41d1a3fbf80210accca80cf432c
Paul Aschmann <paul.aschmann@sap.com>
2016-01-19 21:05:17 -0500

    Modified Gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -6,47 +6,62 @@ var cssnano = require('gulp-cssnano');
 var imagemin = require('gulp-imagemin');
 var del = require('del');
 var runSequence = require('run-sequence');
+const zip = require('gulp-zip');
+var size = require('gulp-filesize');
 
 gulp.task('build', function (callback) {
-  runSequence('clean:dist', 
-    ['clean:dist', 'scripts', 'images', 'fonts', 'xsjslib', 'tests'],
-    callback
-  )
+    runSequence(
+        'clean', 
+        'root',
+        ['scripts', 'images', 'fonts', 'xsjslib', 'tests'],
+        'createzip',
+        callback
+    )
+});
+
+gulp.task('root', function() {
+    return gulp.src(['app/*.*'], { dot: true })
+      .pipe(gulp.dest('dist/'))
 });
 
 gulp.task('scripts', function(){
-  return gulp.src('app/*.html')
-    .pipe(useref())
-    .pipe(gulpIf('*.js', uglify()))
-    // Minifies only if it's a CSS file
-    .pipe(gulpIf('*.css', cssnano()))
-    .pipe(gulp.dest('dist'))
+    return gulp.src('app/*.html')
+      .pipe(useref())
+      .pipe(gulpIf('*.js', uglify()))
+      .pipe(gulpIf('*.css', cssnano()))
+      .pipe(gulp.dest('dist'))
 });
 
 gulp.task('images', function(){
-  return gulp.src('app/img/**/*.+(png|jpg|jpeg|gif|svg)')
-  .pipe(imagemin({
-      // Setting interlaced to true
-      interlaced: true
-    }))
-  .pipe(gulp.dest('dist/img'))
+    return gulp.src('app/img/**/*.+(png|jpg|jpeg|gif|svg)')
+        .pipe(imagemin({
+            interlaced: true
+        }))
+        .pipe(gulp.dest('dist/img'))
 });
 
 gulp.task('fonts', function() {
-  return gulp.src('app/fonts/**/*')
-  .pipe(gulp.dest('dist/fonts'))
+    return gulp.src('app/fonts/**/*')
+      .pipe(gulp.dest('dist/fonts'))
 });
 
 gulp.task('xsjslib', function() {
-  return gulp.src('app/lib/**/*')
-  .pipe(gulp.dest('dist/lib'))
+    return gulp.src('app/lib/**/*')
+      .pipe(gulp.dest('dist/lib'))
 });
 
 gulp.task('tests', function() {
-  return gulp.src('app/tests/**/*')
-  .pipe(gulp.dest('dist/tests'))
+    return gulp.src('app/tests/**/*')
+        .pipe(gulp.dest('dist/tests'))
 });
 
-gulp.task('clean:dist', function() {
-  return del.sync('dist');
+gulp.task('createzip', function(){
+	return gulp.src('dist/**/*')
+		.pipe(zip('syscompare.zip'))
+        .pipe(size())
+		.pipe(gulp.dest(''))
+});
+
+gulp.task('clean', function() {
+    return del.sync(['dist', '.DS_Store', 'app/.DS_Store']);
 });
